db543f4f4f89fd06b3bc50272fc9ddf31dd14080
William Bryce Angell <bryce.angell@resi.io>
2022-07-15 13:46:39 -0500

    setup login form on Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
 import styles from "./Login.module.css";
 
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  const { loginForm } = styles;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+  
+  return (
+    <form className={loginForm} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
 };
 
 export default Login;
