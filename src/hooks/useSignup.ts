7c3afa234f78a5028a7fca3fca0c5e0a96fe7433
William Bryce Angell <bryce.angell@resi.io>
2022-07-15 14:50:48 -0500

    create the useSignup hook

---
+++ src/hooks/useSignup.ts
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { auth } from "../firebase/config";
+
+export const useSignup = () => {
+  const [error, setError] = useState<Error>();
+  const [isPending, setIsPending] = useState(false);
+  const signUp = async (
+    email: string,
+    password: string,
+    displayName: string
+  ) => {
+    setError(undefined);
+    setIsPending(true);
+    try {
+      const userCredential = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      if (!userCredential.user) throw new Error("Could not complete signup");
+      await userCredential.user.updateProfile({ displayName });
+    } catch (e) {
+      setError(e as Error);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, signUp };
+};
