c0bf3bfd88caca9683d5c7d89de607b2240ba6d4
William Bryce Angell <bryce.angell@resi.io>
2022-07-15 14:27:10 -0500

    add firebase

---
+++ src/firebase/configs.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcz5piHCGPVWIqdMqmVKg3PRBNCr8i9LQ",
+  authDomain: "mymoney-29ef6.firebaseapp.com",
+  projectId: "mymoney-29ef6",
+  storageBucket: "mymoney-29ef6.appspot.com",
+  messagingSenderId: "919570927642",
+  appId: "1:919570927642:web:03d2a5bd57dba2c01841a3",
+};
+
+firebase.initializeApp(firebaseConfig);
