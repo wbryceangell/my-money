440908fb783d0e9ce46169f74e495cc5fee198a5
William Bryce Angell <bryce.angell@resi.io>
2022-07-19 15:47:23 -0500

    redirect to correct pages based on auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/home/Home";
@@ -6,7 +6,7 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
       {authIsReady && (
@@ -14,13 +14,16 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/">
-              <Home />
+              {!user && <Redirect to="/login" />}
+              {user && <Home />}
             </Route>
             <Route path="/login">
-              <Login />
+              {!user && <Login />}
+              {user && <Redirect to="/" />}
             </Route>
             <Route path="/signup">
-              <Signup />
+              {!user && <Signup />}
+              {user && <Redirect to="/" />}
             </Route>
           </Switch>
         </BrowserRouter>
