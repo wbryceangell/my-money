8b638d15088508c5530d78b00e0b651dbe5ed6a0
William Bryce Angell <bryce.angell@resi.io>
2022-07-20 10:39:15 -0500

    add document using useFirestore hook

--- src/hooks/useFirestore.ts
+++ src/hooks/useFirestore.ts
@@ -1,18 +1,39 @@
-import { Reducer, useEffect, useReducer, useState } from "react";
+import React, { Reducer, useEffect, useReducer, useState } from "react";
 import { firestore } from "../firebase/config";
 
 type State = {
-  document: firebase.default.firestore.DocumentData | null;
+  document: firebase.default.firestore.DocumentReference | null;
   error: Error | null;
   isPending: boolean;
-  success: boolean | null;
+  success: boolean;
 };
-type ActionType = "";
+type ActionType = "IS_PENDING" | "ADDED_DOCUMENT" | "ERROR";
 type Action = { type: ActionType; payload?: any };
 
 const reducer: Reducer<State, Action> = (state, action) => {
-  const { type } = action;
+  const { type, payload } = action;
   switch (type) {
+    case "IS_PENDING":
+      return {
+        isPending: true,
+        document: null,
+        success: false,
+        error: null,
+      };
+    case "ADDED_DOCUMENT":
+      return {
+        isPending: false,
+        error: null,
+        success: true,
+        document: payload,
+      };
+    case "ERROR":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: payload,
+      };
     default:
       return state;
   }
@@ -22,16 +43,30 @@ const initialState: State = {
   document: null,
   error: null,
   isPending: false,
-  success: null,
+  success: false,
 };
 
-export const useFirestore = (collection: string) => {
+export const useFirestore = (collectionPath: string) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [isCancelled, setIsCancelled] = useState(false);
-  const collectionRef = firestore.collection(collection);
-  collectionRef;
-  const addDocument = (document: firebase.default.firestore.DocumentData) => {};
-  const deleteDocument = (id: string) => {};
+  const collectionRef = firestore.collection(collectionPath);
+
+  const dispatchIfNotCancelled: React.Dispatch<Action> = (action) =>
+    !isCancelled && dispatch(action);
+
+  const addDocument = async (
+    documentData: firebase.default.firestore.DocumentData
+  ) => {
+    dispatchIfNotCancelled({ type: "IS_PENDING" });
+    try {
+      const documentRef = await collectionRef.add(documentData);
+      dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payload: documentRef });
+    } catch (e) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: e });
+    }
+  };
+
+  const deleteDocument = async (id: string) => {};
 
   useEffect(() => () => setIsCancelled(true), []);
   return { addDocument, deleteDocument, state };
